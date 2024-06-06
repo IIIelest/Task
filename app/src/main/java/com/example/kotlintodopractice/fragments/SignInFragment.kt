0a879ffee9915340b4a4332bf3c2e66d7417e4ff
IIIelest <egordrukov08@gmail.com>
2024-06-06 22:52:16 +0300

    first

---
+++ app/src/main/java/com/example/kotlintodopractice/fragments/SignInFragment.kt
@@ -0,0 +1,131 @@
+package com.example.kotlintodopractice.fragments
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.kotlintodopractice.R
+import com.example.kotlintodopractice.databinding.FragmentSignInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.GoogleAuthProvider
+
+class SignInFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: FragmentSignInBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        binding.textViewSignUp.setOnClickListener {
+            navController.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty())
+
+                loginUser(email, pass)
+            else
+                Toast.makeText(context, "Пустые поля не допускаются", Toast.LENGTH_SHORT).show()
+        }
+
+        binding?.btnGoogle?.setOnClickListener{signInWithGoogle() }
+    }
+
+    private fun loginUser(email: String, pass: String) {
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+            if (it.isSuccessful)
+                navController.navigate(R.id.action_signInFragment_to_homeFragment)
+            else
+                Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun signInWithGoogle(){
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {result->
+        if (result.resultCode == Activity.RESULT_OK)
+        {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account:GoogleSignInAccount? = task.result
+            if (account!=null) {
+                updateUI(account)
+            }
+        }
+        else{
+            Toast.makeText(context, "Ошибка входа. Попробойте ещё раз", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                navController.navigate(R.id.action_signInFragment_to_homeFragment)
+            } else {
+                Toast.makeText(context, task.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun init(view: View) {
+        navController = Navigation.findNavController(view)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+}
